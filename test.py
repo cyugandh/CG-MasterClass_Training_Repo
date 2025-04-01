2409f4a3208452d83c7d12f350951df3653f3dbe
cyugandh <ch.yugandhar010@gmail.com>
2025-04-01 20:13:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 print('Hello\nPython')
-def fun(parameters):
-  for num in parameters:
-    print(num, end=' ')
-fun([1,2,3,4,5,6,7,8,9])
+class Tests:
+  def __init__(self,x, y):
+    self._x = x
+    self._y = y
+
+  def addition(self):
+    return self._x + self._y
+
+  def subtraction(slef):
+    return self._x - self._y
+
+  def multiplication(self):
+    return self._x * self._y
+
+  def divison(self):
+    return self._x // self._y
+
+c = Tests(3, 6)
+print(c)
